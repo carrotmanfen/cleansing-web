d07615dcffe7ce8a9abac593f291be94532b323b
Kudsun Khamjamnun <kudsun.k@gmail.com>
2023-10-22 16:33:47 +0700

    set up

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
 import '@/styles/globals.css'
 import Head from 'next/head';
 
-
 export default function App({ Component, pageProps }) {
     return(
         <>
             <Head>
+            <link rel="icon" href="/logo.svg" />
                 <title>Cleansing</title>
             </Head>
 
