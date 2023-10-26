ed2953bbcb1fdae12605e3a42e77143a38fd2777
Fairyfay.k <kanthila.23145@mail.kmutt.ac.th>
2023-10-26 20:28:50 +0700

    update

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,12 @@ import logo from './logo.svg'
 import uploadIcon from './uploadIcon.svg'
 import databaseIcon from './databaseIcon.svg'
 import pic from './pic.svg'
+import doc from './doc.svg'
 
 export{
     logo,
     uploadIcon,
     databaseIcon,
     pic,
+    doc,
 }
---
+++ src/pages/document.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import Image from 'next/legacy/image'
+import { Navbar } from '../components/Navbar'
+import { doc, pic, docTest } from '@/assets'
+
+const document = () => {
+
+    return (
+        <div className='relative w-screen h-full pb-20'>
+            <Navbar />
+            <div className='w-full flex flex-col items-center'>
+                <div className='w-[700px] flex flex-col mt-10 justify-around border-0'>
+                    <p className='text-textPrimary font-kanit text-[24px]'>Why Data Important</p>
+                    <div className='w-[700px] mt-5 flex flex-col justify-center border-0'>
+                        <Image src={doc} width={700} height={500} objectFit='corver' alt="pic" />
+                        <p className='text-textGray text-[16px] mt-2'> Some of description Some of description Some of description Some of description Some of description Some of description Some of description Some of description Some of descriptionSome of descriptionSome of descriptionSome of descriptionSome of description</p>
+                    </div>
+
+                    <p className='text-textPrimary font-kanit text-[24px] mt-5'>Cleansing Data</p>
+                    <div className='w-[700px] mt-5 flex flex-col justify-center border-0'>
+                        <Image src={pic} width={700} height={500} objectFit='contain' alt="pic" />
+                        <p className='text-textGray text-[16px] mt-2'> Some of description Some of description Some of description Some of description Some of description Some of description Some of description Some of description Some of descriptionSome of descriptionSome of descriptionSome of descriptionSome of description</p>
+                    </div>
+
+                    <p className='text-textPrimary font-kanit text-[24px] mt-5'>Profiling of Data</p>
+                    <div className='w-[700px] mt-5 flex flex-col justify-center border-0'>
+                        <Image src={pic} width={700} height={500} objectFit='corver' alt="pic" />
+                        <p className='text-textGray text-[16px] mt-2'> Some of description Some of description Some of description Some of description Some of description Some of description Some of description Some of description Some of descriptionSome of descriptionSome of descriptionSome of descriptionSome of description</p>
+                    </div>
+                </div>
+            </div>
+            <div className='flex flex-col w-full mt-2 font-kanit text-textPrimary text-center items-center'>
+                <button className='px-12 py-2 bg-primary text-white text-[24px] w-fit rounded-2xl mt-8 hover:bg-hoverPrimary'>ทำความสะอาดข้อมูล</button>
+            </div>
+        </div>
+    )
+}
+
+export default document
