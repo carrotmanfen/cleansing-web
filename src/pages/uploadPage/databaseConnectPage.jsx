534234ee7c7edbed6d0ba1b5f22f9f082cf8da96
Fairyfay.k <kanthila.23145@mail.kmutt.ac.th>
2024-01-11 11:34:52 +0700

    enter input data of DatabaseConnectPage

--- src/pages/uploadPage/databaseConnectPage.jsx
+++ src/pages/uploadPage/databaseConnectPage.jsx
@@ -13,7 +13,7 @@ const DatabaseConnectPage = () => {
   const [password, setPassword] = useState('');
   const [database, setDatabase] = useState('');
   const [table, setTable] = useState('');
-  const {error, setError, isPending, showLoading, hideLoading, createProject} = useAddProject()
+  const { error, setError, isPending, showLoading, hideLoading, createProject } = useAddProject()
 
   const handleHostChange = (e) => {
     setHost(e.target.value); // Update the state when the input changes
@@ -40,6 +40,15 @@ const DatabaseConnectPage = () => {
     setError(null)
   };
 
+  const handleEnterKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault(); // Prevent the default behavior of the Enter key (e.g., form submission)
+      if (host && user && database && table) {
+        handleConnect();
+      }
+    }
+  };
+  
   const handleConnect = async () => {
     console.log(host)
     console.log(user)
@@ -80,15 +89,17 @@ const DatabaseConnectPage = () => {
     }
   };
 
-
-
   return (
     <div className="relative w-screen h-full">
       <Navbar />
       <div className="w-full flex flex-col items-center pb-20 ">
-      {error && <div className='w-full flex justify-center absolute mt-10'>
-                    <Alert severity="error" className='w-1/2 font-kanit text-[16px]'>เกิดข้อผิดพลาด - ข้อมูลฐานข้อมูลไม่ถูกต้องหรือเซิฟเวอร์ไม่ตอบสนอง</Alert>
-                </div>}
+        <div className="fixed top-24 z-50 left-1/2 transform -translate-x-1/2 w-1/2">
+          {error &&
+            <Alert severity="error" className="w-full font-kanit text-lg">
+              เกิดข้อผิดพลาด - ข้อมูลฐานข้อมูลไม่ถูกต้องหรือเซิฟเวอร์ไม่ตอบสนอง
+            </Alert>
+          }
+        </div>
         <p className="text-[32px] text-textPrimary font-kanit mt-8">เชื่อมฐานข้อมูล</p>
         <div className='w-[500px] flex flex-col gap-4 mt-8 justify-around font-kanit text-[24px]'>
           <p className="w-full text-left">Host<span className="text-primaryRed"> *</span></p>
@@ -98,6 +109,7 @@ const DatabaseConnectPage = () => {
             placeholder="localhost"
             value={host}
             onChange={handleHostChange}
+            onKeyDown={handleEnterKeyPress}
           />
 
           <p className="w-full text-left">User<span className="text-primaryRed"> *</span></p>
@@ -107,6 +119,7 @@ const DatabaseConnectPage = () => {
             placeholder="root"
             value={user}
             onChange={handleUserChange}
+            onKeyDown={handleEnterKeyPress}
           />
 
           <p className="w-full text-left">Password</p>
@@ -116,6 +129,7 @@ const DatabaseConnectPage = () => {
             placeholder="*******"
             value={password}
             onChange={handlePasswordChange}
+            onKeyDown={handleEnterKeyPress}
           />
 
           <p className="w-full text-left">Database<span className="text-primaryRed"> *</span></p>
@@ -125,6 +139,7 @@ const DatabaseConnectPage = () => {
             placeholder="Database Name"
             value={database}
             onChange={handleDatabaseChange}
+            onKeyDown={handleEnterKeyPress}
           />
 
           <p className="w-full text-left">Table<span className="text-primaryRed"> *</span></p>
@@ -134,20 +149,20 @@ const DatabaseConnectPage = () => {
             placeholder="root"
             value={table}
             onChange={handleTableChange}
+            onKeyDown={handleEnterKeyPress}
           />
 
           <div className="flex flex-row w-full justify-between">
             <Link href={"/uploadPage"}>
               <button className="px-16 py-2 mt-8 bg-gray hover:bg-textGray rounded-2xl">ยกเลิก</button>
             </Link>
-            <button 
-                onClick={handleConnect} 
-                className={`px-16 py-2 mt-8 rounded-2xl  ${host && user && database && table ? 'bg-primary hover:bg-hoverPrimary text-white' : 'bg-gray cursor-not-allowed text-textPrimary'}`}
-                disabled={!host || !user || !database || !table}
+            <button
+              onClick={handleConnect}
+              className={`px-16 py-2 mt-8 rounded-2xl  ${host && user && database && table ? 'bg-primary hover:bg-hoverPrimary text-white' : 'bg-gray cursor-not-allowed text-textPrimary'}`}
             >
-                    ยืนยัน
+              ยืนยัน
             </button>
-            
+
           </div>
         </div>
       </div>
